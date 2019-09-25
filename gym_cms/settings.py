820bfae661723e970e9a47ac937db9369e712f7d
godlike <andayac@gmail.com>
2019-09-24 17:05:24 -0700

    domain added to hosts in settings.py

--- gym_cms/settings.py
+++ gym_cms/settings.py
@@ -30,7 +30,7 @@ STRIPE_API_KEY = env['STRIPE_API_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "gymez.ml", "www.gymez.ml"]
 
 AUTH_USER_MODEL = 'home.User'
 
