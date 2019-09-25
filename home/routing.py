a3118cc3b73da681d78739ce5b7c29b104df5c49
godlike <andayac@gmail.com>
2019-09-24 17:00:14 -0700

    channels re_path -> url

--- home/routing.py
+++ home/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path, path
+from django.conf.urls import url
 
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path(r'ws/home/scan/', consumers.CamConsumer),
+	url('ws/home/scan/', consumers.CamConsumer),
 ]
